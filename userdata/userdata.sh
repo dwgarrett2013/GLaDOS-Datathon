2defad96ca104b840fce837d4400db6e033d788c
David <dwgarrett2013@gmail.com>
2019-02-07 02:05:38 -0500

    adding updates to cft

--- userdata/userdata.sh
+++ userdata/userdata.sh
@@ -11,8 +11,8 @@ rm -F rstudio-server-rhel-1.0.153-x86_64.rpm
 R -e "install.packages('shiny', repos='http://cran.rstudio.com/')"
 wget https://download3.rstudio.org/centos5.9/x86_64/shiny-server-1.5.4.869-rh5-x86_64.rpm
 yum install -y --nogpgcheck shiny-server-1.5.4.869-rh5-x86_64.rpm
-rm -F shiny-server-1.5.4.869-rh5-x86_64.rpm
+#rm -F shiny-server-1.5.4.869-rh5-x86_64.rpm
 
 #add user(s)
 useradd david
-echo david:password | chpasswd
+echo david:thepass | chpasswd
